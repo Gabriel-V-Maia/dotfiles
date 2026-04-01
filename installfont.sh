6578b2e7e48d9f1771c3ae258536a8037d66b6c2
Gabriel Vinícius da Maia <gabriel_v_maia@estudante.sesisenai.org.br>
2026-04-01 08:58:18 -0300

    Add unzip installation check for WSL
    Check for unzip command and install it if missing.

--- installfont.sh
+++ installfont.sh
@@ -1,6 +1,20 @@
 FILENAME="PkgTTC-SGr-IosevkaTerm-34.2.1.zip"
 FONT_DIRECTORY="$HOME/.local/share/fonts"
 
+# wsl doesn't come with ts
+if ! command -v unzip &>/dev/null; then
+    if command -v apt &>/dev/null; then
+        sudo apt install -y unzip
+    elif command -v dnf &>/dev/null; then
+        sudo dnf install -y unzip
+    elif command -v pacman &>/dev/null; then
+        sudo pacman -S --noconfirm unzip
+    else
+        echo "install unzip manually"
+        exit 1
+    fi
+fi
+
 echo "downloading the font from be5invis/Iosevka from github"
 wget "https://github.com/be5invis/Iosevka/releases/download/v34.2.1/$FILENAME"
 
